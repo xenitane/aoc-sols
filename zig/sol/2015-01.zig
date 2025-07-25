dbf2bbbeeaec839dd9952e525cc85c3e0923a581
xenitane <tushar01.tjdsk@gmail.com>
2025-07-26 00:17:11 +0530

    2015-01.zig

---
+++ zig/sol/2015-01.zig
@@ -0,0 +1,65 @@
+const Result = lib.Result(i64, u64);
+fn solve(_: std.mem.Allocator, file_content: []const u8) !Result {
+    var res = Result{ .first = 0, .second = 0 };
+
+    for (file_content, 1..) |byte, idx| {
+        switch (byte) {
+            '(' => {
+                res.first += 1;
+            },
+            ')' => {
+                res.first -= 1;
+                if (res.first < 0 and res.second == 0) {
+                    res.second = idx;
+                }
+            },
+            else => return error.Unreachable,
+        }
+    }
+
+    return res;
+}
+
+const std = @import("std");
+const lib = @import("lib/root.zig");
+const opts = @import("opts");
+
+pub fn main() !void {
+    var da = std.heap.DebugAllocator(.{}).init;
+    defer {
+        if (da.deinit() == std.heap.Check.leak) {
+            @panic("!!!memory leak detected!!!\n");
+        }
+    }
+
+    const ac = da.allocator();
+
+    const file_content = try lib.util.readEntireFile(ac, opts.input_file_path);
+    defer ac.free(file_content);
+
+    const res = try solve(ac, file_content);
+    try res.print(std.io.getStdOut().writer());
+}
+
+test solve {
+    const ally = std.testing.allocator;
+
+    const in_file_content = try lib.util.readEntireFile(ally, opts.test_input_file_path);
+    defer ally.free(in_file_content);
+    const out_file_content = try lib.util.readEntireFile(ally, opts.test_output_file_path);
+    defer ally.free(out_file_content);
+
+    const res = try solve(ally, in_file_content);
+    var string_builder = std.ArrayList(u8).init(ally);
+    defer string_builder.deinit();
+    try res.print(string_builder.writer());
+
+    while (string_builder.getLastOrNull()) |elem| {
+        if (!std.ascii.isWhitespace(elem)) {
+            break;
+        }
+        _ = string_builder.pop();
+    }
+
+    try std.testing.expectEqualStrings(out_file_content, string_builder.items);
+}
