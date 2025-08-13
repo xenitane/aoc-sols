454d2f0219e3ae38a3612a5751be76e32a5dfa87
xenitane <tushar01.tjdsk@gmail.com>
2025-08-14 00:22:12 +0530

    2016-09.hs

---
+++ haskell/sol/2016-09.hs
@@ -0,0 +1,73 @@
+{-# LANGUAGE CPP #-}
+
+import Data.Char (isDigit)
+import Data.Tuple.Extra (both)
+import Lib (exit, pairToStr, safeReadFile, trimTrailing)
+import System.Environment (lookupEnv)
+import System.Exit (ExitCode(ExitFailure), exitWith)
+
+solve :: String -> String
+solve input = pairToStr (first, second)
+  where
+    (first, second) = both (decompress input) (False, True)
+
+decompress :: String -> Bool -> Int
+decompress "" _ = 0
+decompress ('(':rest) nest =
+    times *
+    (if nest
+         then decompress (take len rawRest) True
+         else len) +
+    decompress (drop len rawRest) nest
+  where
+    rawRest = (drop 1 . dropWhile isDigit . drop 1 . dropWhile isDigit) rest
+    times = (read . takeWhile isDigit . drop 1 . dropWhile isDigit) rest :: Int
+    len = (read . takeWhile isDigit) rest :: Int
+decompress (h:rest) nest = 1 + decompress rest nest
+
+inputFilePath :: FilePath
+inputFilePath = "../inputs/" ++ YEAR ++ "-" ++ DAY ++ ".txt"
+
+testInputFilePath :: FilePath
+testInputFilePath = "../test_inputs/" ++ YEAR ++ "-" ++ DAY ++ ".txt"
+
+testOutputFilePath :: FilePath
+testOutputFilePath = "../test_outputs/" ++ YEAR ++ "-" ++ DAY ++ ".txt"
+
+runNormalMode :: IO ()
+runNormalMode = do
+    input <- safeReadFile inputFilePath
+    (putStr . solve) input
+
+runTestMode :: IO ()
+runTestMode = do
+    input <- safeReadFile testInputFilePath
+    expectedIO <- safeReadFile testOutputFilePath
+    let expected = trimTrailing expectedIO
+    let actual = (trimTrailing . solve) input
+    if actual == expected
+        then putStrLn "test passed"
+        else do
+            putStrLn "test failed"
+            putStrLn "Expected:"
+            putStrLn "--------------"
+            putStrLn expected
+            putStrLn "--------------"
+            putStrLn "Got:"
+            putStrLn "--------------"
+            putStrLn actual
+            putStrLn "--------------"
+            exit 1
+
+main :: IO ()
+#if defined YEAR && defined DAY
+main = do
+    testMode <- lookupEnv "TEST_MODE"
+    case testMode of
+        Just "1" -> runTestMode
+        _ -> runNormalMode
+#else
+main = do
+    putStrLn "essential variables not defined"
+    exit 1
+#endif
