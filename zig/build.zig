1ae29045a328101c0f979fe44656bf65f1f12e65
xenitane <tushar01.tjdsk@gmail.com>
2025-09-07 03:08:26 +0530

    moved stuff into dedicated dir

--- zig/build.zig
+++ zig/build.zig
@@ -7,19 +7,19 @@ pub fn build(b: *Build) !void {
     const exe_name = b.option([]const u8, "exe", "") orelse @panic("needed");
 
     const exe_mod = b.createModule(.{
-        .root_source_file = b.path("main.zig"),
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const exe = b.addExecutable(.{ .root_module = exe_mod, .name = exe_name });
 
-    const install_step = b.addInstallArtifact(exe, .{});
+    b.installArtifact(exe);
     const run_exe_step = b.addRunArtifact(exe);
+    run_exe_step.step.dependOn(b.getInstallStep());
 
     const run_step = b.step("run", "run the built executable");
 
-    run_step.dependOn(&install_step.step);
     run_step.dependOn(&run_exe_step.step);
 
     const run_exe_unit_test_step = b.addRunArtifact(b.addTest(.{ .root_module = exe_mod }));
